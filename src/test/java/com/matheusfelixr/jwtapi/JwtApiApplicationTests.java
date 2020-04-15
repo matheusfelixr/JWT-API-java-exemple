f7956c9d15324a0502e6105ffd542db885c12c6e
Matheus Felix <matheusfelixr@hotmail.com>
2020-04-14 23:42:44 -0300

    InitialComit

---
+++ src/test/java/com/matheusfelixr/jwtapi/JwtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusfelixr.jwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
