53366d7cf7ab552c6e90adcea431b061a445d6c6
Matheus Felix <matheusfelixr@hotmail.com>
2020-04-19 00:41:15 -0300

    Alteração CrossOrigin

--- src/main/java/com/matheusfelixr/jwtapi/controller/AuthenticatedRouteController.java
+++ src/main/java/com/matheusfelixr/jwtapi/controller/AuthenticatedRouteController.java
@@ -1,10 +1,12 @@
 package com.matheusfelixr.jwtapi.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/authenticated-route")
 public class AuthenticatedRouteController {
 	
